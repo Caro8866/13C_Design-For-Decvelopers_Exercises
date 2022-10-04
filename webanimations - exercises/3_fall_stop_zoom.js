3e1a2f3120a83b497b0b281bcadfe45db092d839
Caroline Thostrup <caro342h@stud.kea.dk>
2022-10-04 15:35:07 +0200

    ex 3 part 1 done

--- webanimations - exercises/3_fall_stop_zoom.js
+++ webanimations - exercises/3_fall_stop_zoom.js
@@ -1,17 +1,33 @@
-"use strict"
+"use strict";
 
 const ball = document.querySelector("#ball");
 
 // Create keyframes and properties for falling and zoom
+const fallingProperties = {
+  duration: 2000,
+  iterations: Infinity,
+  easing: "ease-in-out",
+};
 
+const fallingKeyframes = [{ transform: "translateY(-20vw)" }, { transform: "translateY(65vw)" }];
+
+const zoomProperties = {
+  duration: 1000,
+  iterations: 1,
+  fill: "forwards",
+  composite: "add",
+};
+
+const zoomKeyframes = [{ transform: "scale(1)" }, { transform: "scale(0)" }];
 // register click
-
+ball.addEventListener("click", ballClicked);
 // start falling animation
+const animation = ball.animate(fallingKeyframes, fallingProperties);
 
 function ballClicked() {
   console.log("Ball clicked!");
   // pause falling animation
-
+  animation.pause();
   // start zoom-animation
-
+  const zoomAnimation = ball.animate(zoomKeyframes, zoomProperties);
 }
