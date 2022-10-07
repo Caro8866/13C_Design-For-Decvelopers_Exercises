46949a504a812223c15e9e7a707f02541a6ba316
Caroline Thostrup <caro342h@stud.kea.dk>
2022-10-07 10:33:30 +0200

    exercise 2 done - boucning balls

--- onemotion_exercise/multiballs.js
+++ onemotion_exercise/multiballs.js
@@ -1,1 +1,4 @@
 "use strict";
+import { animate, stagger } from "https://cdn.skypack.dev/motion";
+
+animate(".ball", { translateY: -200 }, { duration: 2, delay: stagger(0.2), repeat: Infinity, direction: "alternate" });
