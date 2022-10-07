279c7fbdaf598371effbd4fcd830c5789f51face
Caroline Thostrup <caro342h@stud.kea.dk>
2022-10-07 08:54:31 +0200

    ex 4 done

--- webanimations - exercises/4_more_keyframes.js
+++ webanimations - exercises/4_more_keyframes.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const ball = document.querySelector("#ball");
+
+const fourPointKeyFrames = [{ transform: "translate(42vw,0vh)" }, { transform: "translate(85vw,20vw)" }, { transform: "translate(42vw,41vw)" }, { transform: "translate(0vw,20vw)" }];
+
+fourPointKeyFrames.push(fourPointKeyFrames[0]);
+const fourPointProperties = {
+  duration: 3000,
+  iterations: Infinity,
+};
+
+const fourPointAnimation = ball.animate(fourPointKeyFrames, fourPointProperties);
