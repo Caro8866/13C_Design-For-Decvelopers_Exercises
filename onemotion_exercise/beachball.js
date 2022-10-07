f3208bca8ce52f0d570d77485298312e431511ce
Caroline Thostrup <caro342h@stud.kea.dk>
2022-10-07 10:17:56 +0200

    oneMotion exercise 1 done

--- onemotion_exercise/beachball.js
+++ onemotion_exercise/beachball.js
@@ -1,1 +1,6 @@
 "use strict";
+import { animate } from "https://cdn.skypack.dev/motion";
+
+animate(".ball", { translateX: 380 }, { duration: 2 });
+animate(".ball", { rotate: 200 }, { duration: 2 });
+animate(".ball", { scale: 0 }, { duration: 0.5, delay: 1.8 });
