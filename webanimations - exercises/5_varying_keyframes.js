789a9a1eb6b9eb84aa81f78f1258fcad26b36f59
Caroline Thostrup <caro342h@stud.kea.dk>
2022-10-07 09:21:22 +0200

    ex 5

--- webanimations - exercises/5_varying_keyframes.js
+++ webanimations - exercises/5_varying_keyframes.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const stageGbcrObj = document.querySelector("#stage").getBoundingClientRect();
+const ballGbcrObj = document.querySelector("#ball").getBoundingClientRect();
+
+const centerX = (stageGbcrObj.width - ballGbcrObj.width) / 2;
+const rightX = stageGbcrObj.width - ballGbcrObj.width;
+const centerY = (stageGbcrObj.height - ballGbcrObj.height) / 2;
+const bottomY = stageGbcrObj.height - ballGbcrObj.height;
+
+console.log(centerX, centerY, rightX, bottomY);
+
+const fourPointKeyFrames = [{ transform: `translate(${centerX}px,0vh)` }, { transform: `translate(${rightX}px, ${centerY}px)` }, { transform: `translate(${centerX}px, ${bottomY}px)` }, { transform: `translate(0vw,${centerY}px)` }];
+
+fourPointKeyFrames.push(fourPointKeyFrames[0]);
+const fourPointProperties = {
+  duration: 3000,
+  iterations: Infinity,
+};
+
+const fourPointAnimation = ball.animate(fourPointKeyFrames, fourPointProperties);
