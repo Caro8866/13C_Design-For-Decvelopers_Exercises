a7fe8b3cd00c1cf36daced50086235c5fab834ce
Caroline Thostrup <caro342h@stud.kea.dk>
2022-10-07 10:21:59 +0200

    combined rotate and translate

--- onemotion_exercise/beachball.js
+++ onemotion_exercise/beachball.js
@@ -1,6 +1,5 @@
 "use strict";
 import { animate } from "https://cdn.skypack.dev/motion";
 
-animate(".ball", { translateX: 380 }, { duration: 2 });
-animate(".ball", { rotate: 200 }, { duration: 2 });
+animate(".ball", { translateX: 380, rotate: 200 }, { duration: 2 });
 animate(".ball", { scale: 0 }, { duration: 0.5, delay: 1.8 });
